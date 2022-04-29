b047883fdc8d4c021e9e490ac2b7ac66f369b8af
Kaashyam <103822304+Kaashyam@users.noreply.github.com>
2022-04-29 17:30:46 +0530

    Add files via upload

---
+++ LearnDatatypes.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class LearnDatatypes {
+// Primitive Data types - Car
+// Syntax - Datatype variableName = value;
+	byte audioVolume = 10;
+	short internalStorage = 24000;
+	int locationLatitiude = 987654321;
+	long engineNumber = 9786543210L;
+	float pixelSize = 2.4586F;
+	double kernalNumber = 122345.8765489;
+	char modelName = 'A';
+	boolean isAcSwitchedOn = false;
+	
+
+}
